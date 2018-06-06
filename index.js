36df015731c6264d73402f8b4b27c25f8de4e076
teralove <terascripts@gmail.com>
2018-06-05 22:24:19 -0400

    update

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 module.exports = function BlockGoldBots(dispatch) {
     
     const keywords = ['M M O'];
-    
-    let inVelika = false;
-    
-    dispatch.hook('S_LOAD_TOPO', 3, (event) => {
-        inVelika = event.zone == 1 ? true : false;
-    });
-    
-    dispatch.hook('S_CHAT', 1, {order: 100}, (event) => {
-        if (!inVelika) return;
-        
+
+    dispatch.hook('S_CHAT', 1, (event) => {
         for (let i = 0; i < keywords.length; i++) {
             if (event.message.includes(keywords[i])) {
                 return false;
