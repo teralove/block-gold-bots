666026b3ac87909f90f9186350d1a3bb568d79bc
teralove <terascripts@gmail.com>
2018-06-09 05:49:03 -0400

    update 1.1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function BlockGoldBots(dispatch) {
     
-    const keywords = ['M M O'];
+    const keywords = ['M M O', 'M M 0'];
 
     dispatch.hook('S_CHAT', 1, (event) => {
         for (let i = 0; i < keywords.length; i++) {
